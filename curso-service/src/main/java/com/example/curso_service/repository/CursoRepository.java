4fb6e63f3c5639020c75c74e762c1c9944daacb3
VAlejandro22 <victorcamacho8762@gmail.com>
2025-01-27 22:39:39 -0500

    delete

--- curso-service/src/main/java/com/example/curso_service/repository/CursoRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.curso_service.repository;
-
-import com.example.curso_service.model.Curso;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CursoRepository extends JpaRepository<Curso, Long> {}
--- material-service/src/main/java/com/example/material_service/repository/MaterialRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.material_service.repository;
-
-
-import com.example.material_service.model.Material;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MaterialRepository extends JpaRepository<Material, Long> {
-
-}
